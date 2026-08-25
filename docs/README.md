462f8b011c679d4e82d246d231a1c793000b18ee
Wang Yujie <wangyujie54321@gmail.com>
2026-08-25 21:37:29 +0800

    feat: Rename chatbot to Johnny and add greeting/farewell message
      Rename Duke class to Johnny and update all references in README files.
      Add startup banner, greeting, and farewell output on exit.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Johnny User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Johnny.java
@@ -0,0 +1,19 @@
+public class Johnny {
+    private static final String LINE = "____________________________________________________________";
+    private static final String BANNER = "     _       _                       \n"
+            + "    | | ___ | |__  _ __  _ __  _   _ \n"
+            + " _  | |/ _ \\| '_ \\| '_ \\| '_ \\| | | |\n"
+            + "| |_| | (_) | | | | | | | | | | |_| |\n"
+            + " \\___/ \\___/|_| |_|_| |_|_| |_|\\__, |\n"
+            + "                                |___/ \n";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.print(BANNER);
+        System.out.println("Hello! I'm Johnny.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye bye! See you again soon.");
+        System.out.println(LINE);
+    }
+}
