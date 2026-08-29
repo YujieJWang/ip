53f4301bc21339935d0c7f70ba059f120820a61d
Wang Yujie <wangyujie54321@gmail.com>
2026-08-29 20:50:04 +0800

    refactor: Convert Johnny from static main to instance-based design
    Replace the static main() monolith with a constructor that initialises
    Ui, Storage, and TaskList, and a run() method that drives the command
    loop. main() is now a one-liner: new Johnny(filePath).run(). This
    makes Johnny instantiable and testable, and removes the need to pass
    ui/storage/tasks as parameters to saveTasks().

--- src/main/java/Johnny.java
+++ src/main/java/Johnny.java
@@ -1,8 +1,27 @@
 import java.io.IOException;
 
+/**
+ * Main application class for the Johnny chatbot.
+ * Orchestrates the UI, storage, task list, and parser components.
+ */
 public class Johnny {
 
-    private static void saveTasks(Storage storage, TaskList tasks, Ui ui) {
+    private final Ui ui;
+    private final Storage storage;
+    private TaskList tasks;
+
+    public Johnny(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (Exception e) {
+            ui.showLoadingError();
+            tasks = new TaskList();
+        }
+    }
+
+    private void saveTasks() {
         try {
             storage.save(tasks);
         } catch (IOException e) {
@@ -10,17 +29,8 @@ public class Johnny {
         }
     }
 
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        Storage storage = new Storage("./data/johnny.txt");
-        TaskList tasks;
-        try {
-            tasks = new TaskList(storage.load());
-        } catch (Exception e) {
-            ui.showLoadingError();
-            tasks = new TaskList();
-        }
-
+    /** Runs the main command loop until the user says bye or input ends. */
+    public void run() {
         ui.showGreeting();
 
         while (ui.hasNextLine()) {
@@ -41,34 +51,34 @@ public class Johnny {
                     int markIndex = Parser.parseTaskIndex(arguments, tasks.size());
                     tasks.get(markIndex).markAsDone();
                     ui.showTaskMarked(tasks.get(markIndex));
-                    saveTasks(storage, tasks, ui);
+                    saveTasks();
                     break;
                 case UNMARK:
                     int unmarkIndex = Parser.parseTaskIndex(arguments, tasks.size());
                     tasks.get(unmarkIndex).markAsNotDone();
                     ui.showTaskUnmarked(tasks.get(unmarkIndex));
-                    saveTasks(storage, tasks, ui);
+                    saveTasks();
                     break;
                 case DELETE:
                     int deleteIndex = Parser.parseTaskIndex(arguments, tasks.size());
                     Task removed = tasks.delete(deleteIndex);
                     ui.showTaskDeleted(removed, tasks.size());
-                    saveTasks(storage, tasks, ui);
+                    saveTasks();
                     break;
                 case TODO:
                     tasks.add(Parser.parseTodo(arguments));
                     ui.showTaskAdded(tasks.get(tasks.size() - 1), tasks.size());
-                    saveTasks(storage, tasks, ui);
+                    saveTasks();
                     break;
                 case DEADLINE:
                     tasks.add(Parser.parseDeadline(arguments));
                     ui.showTaskAdded(tasks.get(tasks.size() - 1), tasks.size());
-                    saveTasks(storage, tasks, ui);
+                    saveTasks();
                     break;
                 case EVENT:
                     tasks.add(Parser.parseEvent(arguments));
                     ui.showTaskAdded(tasks.get(tasks.size() - 1), tasks.size());
-                    saveTasks(storage, tasks, ui);
+                    saveTasks();
                     break;
                 case UNKNOWN:
                     throw new JohnnyException("I'm sorry, but I'm not too sure what that means :(");
@@ -83,5 +93,9 @@ public class Johnny {
         }
 
         ui.close();
+    }
+
+    public static void main(String[] args) {
+        new Johnny("./data/johnny.txt").run();
     }
 }
